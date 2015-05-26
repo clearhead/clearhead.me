b7a7ea0e1e87cf20a8b1951fc79a0faadaf1e60a
Tom Fuertes <tomfuertes@gmail.com>
2015-05-26 16:01:50 -0500

    staging

--- bin/stage.sh
+++ bin/stage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-git remote add production git@git.wpengine.com:production/clearhead.git
-git push production master
+git remote add staging git@git.wpengine.com:staging/clearhead.git
+git push staging master
