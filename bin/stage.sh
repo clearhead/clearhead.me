783d0cc2166d874186d1a51960aaa2abc8f4966e
glyyynn <glynnjordan.jr@gmail.com>
2015-06-02 15:48:07 -0500

    staging update

--- bin/stage.sh
+++ bin/stage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-git remote add staging git@git.wpengine.com:staging/clearhead.git
-git push -f staging master
+git remote add stage git@git.wpengine.com:staging/clearhead.git
+git push -f stage HEAD:master
