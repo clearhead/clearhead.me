b6aef460518edb1c3e25d7f08a54896e12a80b8d
glyyynn <glynnjordan.jr@gmail.com>
2015-06-15 16:12:52 -0500

    adjusting text position, add hover color

--- wp-content/themes/clearhead/page-home.php
+++ wp-content/themes/clearhead/page-home.php
@@ -20,7 +20,8 @@ get_header(); ?>
 			<div id="opticon">
 			<div class="callout bg-light-green center py4 clearfix">
 				<div class="container">
-					<h1 class="medium">We're at Opticon</h1><h3 class="medium"> - Nominated for 5 Optie Awards including Partner of the Year</h3><br />
+					<h1 class="medium">We're at Opticon</h1><br />
+					<h3 class="medium"> Nominated for 5 Optie Awards including Partner of the Year</h3><br />
 					<h3 class="medium"><em><a href="http://clearhead.me/all-about-opticon-2015">here’s how you can meet us.</a></em></h3>
 				</div>
 			</div>
