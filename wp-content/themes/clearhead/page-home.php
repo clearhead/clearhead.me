cf3424067efec9d143c17539cf09346baf27072b
Tom Fuertes <tomfuertes@gmail.com>
2015-06-19 08:46:19 -0700

    partialize

--- wp-content/themes/clearhead/page-home.php
+++ wp-content/themes/clearhead/page-home.php
@@ -17,25 +17,7 @@ get_header(); ?>
 
       <?php while ( have_posts() ) : the_post(); ?>
 
-      <!-- opticon promo -->
-      <div id="opticon">
-        <div class="callout bg-light-green center py4 clearfix">
-          <div class="container">
-            <h1 class="medium">We're at Opticon</h1><br />
-            <h3 class="medium"> Nominated for 5 Optie Awards including Partner of the Year</h3><br />
-            <h3 class="medium"><em><a href="http://clearhead.me/all-about-opticon-2015/">here’s how you can meet us</a></em></h3>
-          </div>
-        </div>
-      </div>
-      <style> /*bypass so it caches*/
-        .bg-light-green { background: #acd69b; color: #225e2f; }
-        #opticon a { color: #225e2f; text-decoration: underline; }
-        #opticon a:hover { opacity: 0.6; }
-        #opticon h1 { font-weight: 2000; }
-        #opticon h1,
-        #opticon h3 { display: inline-block; }
-        #opticon .medium { margin: 0; }
-      </style><!-- /opticon promo -->
+      <?php include 'opticon-banner.php'; ?>
 
       <div class="hero bg-teal clearfix py4">
 
