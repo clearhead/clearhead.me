aed76d946a1a1df19e5182f9d343304caa3ce353
Devin Price <devin@wptheming.com>
2015-08-08 17:13:25 -0500

    #12 Experiments with universal header.

---
+++ wp-content/themes/clearhead/partials/header-blog.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Displays a universal blog header
+ *
+ * @package Clearhead
+ */
+?>
+
+<div class="header-blog">
+	<div class="col-width">
+		<h2>clearhead blog</h2>
+	</div>
+</div>
