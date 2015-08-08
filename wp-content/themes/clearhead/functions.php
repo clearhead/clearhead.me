a14361e4be0e692390891dd79ed20e046d22ed63
Devin Price <devin@wptheming.com>
2015-08-08 16:48:06 -0500

    #16 Remove comment-reply script since disqus is in use.

--- wp-content/themes/clearhead/functions.php
+++ wp-content/themes/clearhead/functions.php
@@ -104,21 +104,16 @@ function clearhead_scripts() {
 
 	wp_enqueue_script( 'clearhead-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	if ( is_front_page() ){
+	if ( is_front_page() ) :
 		wp_enqueue_script( 'clearhead-navigation-home', get_template_directory_uri() . '/js/home.nav.js', array(), '20120206', true );
-	}
+	endif;
 
 	wp_enqueue_script( 'clearhead-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-
-	if ( !is_front_page() ) {
+	if ( ! is_front_page() ) :
 		wp_enqueue_style( 'prism-css', get_template_directory_uri() . '/bower_components/prism/themes/prism-okaidia.css', array(), '20150629');
 		wp_enqueue_script( 'prism-js', get_template_directory_uri() . '/bower_components/prism/prism.js', array(), '20150629', true );
-	}
-
+	endif;
 
 }
 add_action( 'wp_enqueue_scripts', 'clearhead_scripts' );
