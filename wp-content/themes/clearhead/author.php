aa208fc19d408b18afb288e48849d70b903f5a87
Devin Price <devin@wptheming.com>
2016-02-18 12:16:35 -0600

    Update author tempalte.

--- wp-content/themes/clearhead/author.php
+++ wp-content/themes/clearhead/author.php
@@ -19,6 +19,13 @@ get_header(); ?>
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
 
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
 			<?php if ( have_posts() ) : ?>
 
 				<?php /* Start the Loop */ ?>
