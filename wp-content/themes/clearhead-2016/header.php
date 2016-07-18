33f1d0696c93466667291ee2672d50bf1e35d0f1
Joel Farris <joel@return.tc>
2016-07-18 08:01:49 -0500

    Finishing mobile nav

--- wp-content/themes/clearhead-2016/header.php
+++ wp-content/themes/clearhead-2016/header.php
@@ -18,8 +18,14 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+	<?php /*
 	<script src="//use.typekit.net/jlb8kqs.js"></script>
 	<script>try{Typekit.load();}catch(e){}</script>
+	*/ ?>
+
+	<script src="https://use.typekit.net/erk8dkx.js"></script>
+	<script>try{Typekit.load();}catch(e){}</script>
+
 	<meta name="google-site-verification" content="rOVbTCkNzfmtGVaB3SNC3RQvDzAq88fhCv8fTG4mBbY" />
 
 	<?php wp_head(); ?>
@@ -31,51 +37,53 @@
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'clearhead-2016' ); ?></a>
 
 	<header role="banner">
-		<div class="container">
-			<?php if ( is_front_page() ): ?>
-				<a class="logo" href="#main" rel="home">
-			<?php else: ?>
-				<a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-			<?php endif; ?>
+		<div class="main-header">
+			<div class="container">
+				<?php if ( is_front_page() ): ?>
+					<a class="logo" href="#main" rel="home">
+				<?php else: ?>
+					<a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<?php endif; ?>
 
-				<figure>
-					<div class="icon"></div>
-					<figcaption>Clearhead</figcaption>
-				</figure>
-			</a>
-			<div class="menu-desktop">
-				<nav role="navigation">
-					<?php /*
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'clearhead-2016' ); ?></button>
-					*/ ?>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav>
+					<figure>
+						<div class="icon"></div>
+						<figcaption>Clearhead</figcaption>
+					</figure>
+				</a>
+				<div class="menu-desktop">
+					<nav role="navigation">
+						<?php /*
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'clearhead-2016' ); ?></button>
+						*/ ?>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</nav>
+				</div>
+				<a href="#show-menu" class="menu-toggle-mobile">
+					<figure>
+						<figcaption>Menu</figcaption>
+						<div class="icon"><span>+</span></div>
+					</figure>
+				</a>
 			</div>
-			<a href="#show-menu" class="menu-toggle-mobile">
-				<figure>
-					<div class="icon"><span>+</span></div>
-					<figcaption>Menu</figcaption>
-				</figure>
-			</a>
 		</div>
 		<div class="menu-mobile">
 			<div class="container">
 				<nav role="navigation">
 			        <div class="menu-main-menu-container">
 			            <ul class="menu">
-			                <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-6">
+			                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6">
 			                    <a href="/#what">Services</a>
 			                </li>
-			                <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-8">
+			                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8">
 			                    <a href="/#with">Process</a>
 			                </li>
-			                <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-7">
+			                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7">
 			                    <a href="/#who">Team</a>
 			                </li>
-			                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11">
-			                    <a href="http://clearhead.dev/blog/">Blog</a>
+			                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11 menu-item-blog">
+			                    <a href="/blog/">Blog</a>
 			                </li>
-			                <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-9">
+			                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9">
 			                    <a href="/#contact">Contact</a>
 			                </li>
 			            </ul>
