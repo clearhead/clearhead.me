c4caaf85d569383180cece0d88086de02ac74a08
Joel Farris <joel@return.tc>
2016-07-18 17:26:45 -0500

    adding remaining homepage images

---
+++ wp-content/themes/clearhead-2016/img/icons/target.svg.php
@@ -0,0 +1,13 @@
+<svg width="18px" height="18px" viewBox="0 0 18 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" opacity="0.5">
+        <g transform="translate(-422.000000, -6726.000000)" stroke-width="2" stroke="#32C4F5">
+            <g transform="translate(200.000000, 6560.000000)">
+                <g transform="translate(223.000000, 167.000000)">
+                    <polyline points="9.86363636 0 16 0 16 6.13636364"></polyline>
+                    <path d="M15.3181818,0.681818182 L6.45454545,9.54545455"></path>
+                    <polyline points="15 11.8603896 15 16 0 16 0 1 4.13961039 1"></polyline>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
