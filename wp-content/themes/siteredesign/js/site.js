47d81de2fa84be904a278db07f312fb3ebe8f67b
Julian M. Jones <julian@return.tc>
2016-08-16 12:18:52 -0500

    Merging in changes

--- wp-content/themes/siteredesign/js/site.js
+++ wp-content/themes/siteredesign/js/site.js
@@ -41,3 +41,20 @@ window.twttr = (function(d, s, id) {
 
    return t;
   }(document, "script", "twitter-wjs"));
+
+function windowPopup(url, width, height) {
+  // Calculate the position of the popup so
+  // it’s centered on the screen.
+  var left = (screen.width / 2) - (width / 2),
+  top = (screen.height / 2) - (height / 2);
+  window.open(
+  url,
+  "",
+  "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+  );
+  }
+  jQuery(".js-social-share").on("click", function(e) {
+  e.preventDefault();
+  windowPopup(jQuery(this).attr("href"), 500, 300);
+});
+
