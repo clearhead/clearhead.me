ba58b697d1006e5f134c2fdfc1a66eb1edead3d1
DIR\edison.g.seas.morua <edison.g.seas.morua@accenture.com>
2017-12-26 14:34:59 -0600

    - Adding subtitle custom field call in the post list page - Adding custom excerpt text for new template using intro testimony for post list page

--- wp-content/themes/clearhead-2017/template-parts/content.php
+++ wp-content/themes/clearhead-2017/template-parts/content.php
@@ -20,17 +20,54 @@
 		$subtitle = get_field('subtitle');
 		if ($subtitle == NULL) {
 			$subtitle = '';
+		} else {
+			$subtitle = ': ' . $subtitle;
 		}
 
-		$header_title = get_the_title() . ' ' . $subtitle;
+		$header_title = get_the_title() . $subtitle;
+
 	?>
-		
 	<div class="archive-entry-header">
 		<h1 class="entry-title"><a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark"><?php echo $header_title ?></a></h1>
 	</div><!-- .archive-entry-header -->
 
 	<div class="entry-content entry-content-archive">
-		<?php the_excerpt(); ?>
+		<?php
+			// get the excerpt, if there isn't, then take testimony custom fields and it.
+			$c_excerpt = strlen(get_the_excerpt());
+
+			if ($c_excerpt !== 0) {
+			 	the_excerpt();
+			} else if ($c_excerpt == 0) {
+		 		if( have_rows('intro_section') ) {
+		 			while( have_rows('intro_section') ) : the_row();
+						if( get_row_layout() == 'testimonial_block'):
+							// get testimony from testimonial in intro section
+							$testimony_words_count = substr_count(get_sub_field('testimony'), ' ');
+							$max_words = 55;
+							
+							//spit and add the post link if content has more than 55 words
+							if ($testimony_words_count < $max_words):
+							?>
+								<p><?php the_sub_field('testimony'); ?></p>
+							<?php 
+							else:
+								// get the testimony as a string
+								$testimony_content = get_sub_field('testimony');
+								// split the string in words until it gets to the max amount of words
+								$custom_ex = explode(" ", $testimony_content, $max_words);
+								// take out the rest of the text
+								array_pop($custom_ex);
+								// display the text result and the post link
+							?>
+								<p><?php echo join(' ', $custom_ex) ?>...  <a href="<?php echo get_permalink(); ?>">Read More »</a></p>
+							<?php 
+							endif;
+						endif;
+					endwhile;
+		 		}
+			 }
+		 ?>
 	</div><!-- .entry-content -->
 
 	<div class="author-meta clearfix">
@@ -40,5 +77,4 @@
 			<?php clearhead_2016_posted_on(); ?>
 		</div>
 	</div>
-
 </article>
